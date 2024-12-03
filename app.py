f93e4ea165e094454c7b394a6046453ee05a1052
Vert_MawIIGen <wijayaarchie@gmail.com>
2024-12-03 23:42:14 +1100

    Submitted project.

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ app.config['SECRET_KEY'] = "24352afjhljaskdf"
 
 app.secret_key = os.environ.get('SECRET_KEY')
 
-
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
@@ -48,7 +47,6 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(80), nullable=False)
 
 
-
 # creates the form for registering
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
@@ -72,12 +70,6 @@ class LoginForm(FlaskForm):
 def homepage():
     return render_template("front_page.html")
 
-
-@app.route("/base")
-def base():
-    return render_template("base.html")
-
-
 @app.route("/home", methods=["GET", "POST"])
 @login_required
 def auth_home():
@@ -93,7 +85,7 @@ def login():
     if form.validate_on_submit():  # check if user submitted the form
         user = User.query.filter_by(username=form.username.data).first()  # check username validity
         if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):  # check hashed password
+            if bcrypt.check_password_hash(user.password, form.password.data):  # check hashed password matches
                 login_user(user)
                 flash("Login successful.")
                 return redirect(url_for('auth_home', username=form.username.data))  # return homepage
@@ -124,17 +116,19 @@ def register():
 
     return render_template("register.html", form=form)
 
+
 @app.route("/quiz1", methods=["GET", "POST"])
 @login_required
 def firstquiz():
     question_index = session.get("question_index", 1)
+    # check if last question has been done
     if question_index >= len(all_questions):
         session["question_index"] = 1
         return redirect(url_for("results"))
 
+    # check if variables have been created in session
     if session.get("score", None) == None:
         session["score"] = 0
-
     if session.get("questions_correct", None) == None:
         session["questions_correct"] = []
 
@@ -142,30 +136,32 @@ def firstquiz():
     allOptions = current_question[1:-1]
 
     if request.method == "POST":
-        selected_answer = int(request.form.get("answer"))
-        user_answer = chr(selected_answer + 65)
-        print(user_answer, current_question[-1])
-        session["question_index"] = question_index + 1
-        if user_answer != current_question[-1]:
-            session["questions_correct"].append(False)
-            return render_template("feedback.html", feedback=f"Correct Answer is {current_question[-1]}")
-        else:
-            session["score"] += 1
-            session["questions_correct"].append(True)
-            return redirect(url_for("firstquiz"))
-    print(session["score"])
-    return render_template("quiztesting.html", question=current_question[0], options=allOptions, question_num=question_index)
+        # check if user entered something
+        if request.form.get("answer") != None:
+            selected_answer = int(request.form.get("answer"))
+            user_answer = chr(selected_answer + 65)
+            session["question_index"] = question_index + 1
+            if user_answer != current_question[-1]:
+                session["questions_correct"].append(False)
+                return render_template("feedback.html", feedback=f"Correct Answer is {current_question[-1]}")
+            else:
+                session["score"] += 1
+                session["questions_correct"].append(True)
+                return redirect(url_for("firstquiz"))
+    return render_template("quiz1.html", question=current_question[0], options=allOptions,
+                           question_num=question_index)
 
 
 @app.route("/results", methods=["GET", "POST"])
 def results():
     current_score = session.get("score")
     session["latest_score"] = current_score
-    session["score"] = 0
-    print(session["questions_correct"])
+    session["score"] = 0  # set score to 0 for next quiz
     correct_questions = session["questions_correct"]
     session["questions_correct"] = []
-    return render_template("quiz_results.html", score=current_score, corrects=correct_questions, num_questions=len(all_questions) - 1)
+    return render_template("quiz_results.html", score=current_score, corrects=correct_questions,
+                           num_questions=len(all_questions) - 1)
+
 
 @app.route("/web_programming")
 @login_required
@@ -178,15 +174,18 @@ def web_programming():
 def data_transfers():
     return render_template("data_transfer.html")
 
+
 @app.route("/web_security")
 @login_required
 def web_security():
     return render_template("web_security.html")
 
+
 @app.route("/big_data_accessibility")
 @login_required
 def big_data_accessibility():
     return render_template("big_data_accessibility.html")
+
 
 @app.route("/web_development")
 @login_required
@@ -197,8 +196,13 @@ def web_development():
 @app.route("/glossary")
 @login_required
 def glossary():
-    print(all_words)
     return render_template("glossary.html", word_list=all_words)
+
+
+# @app.route("/forum")
+# @login_required
+# def forum():
+#     return "Forum page."
 
 
 if __name__ == '__main__':
